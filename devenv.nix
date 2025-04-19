8ec44bd30939b8aefbe818516615c64108caead4
DamonPalovaara <dpalovaa@nmu.edu>
2025-04-19 12:23:38 -0400

    Initial commit

---
+++ devenv.nix
@@ -0,0 +1,21 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  languages.cplusplus.enable = true;
+  
+  packages = with pkgs; [
+    git
+    wget
+    gdb
+    zip
+    jq
+    python3
+    zstd
+    libpulseaudio
+    pkgconf
+  ];
+
+  enterShell = ''
+    export RACK_DIR="./Rack-SDK"
+  '';
+}
